38ca5876fd87cd9508e948bde76bead7fc461048
liuzhe <zhethink@gmail.com>
2018-09-21 16:59:28 +0800

    npm i element-ui -S

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="topBar">
-    我是TopBar
+    <div class="log">
+      MyResumer
+    </div>
+    <div class="action">
+      <el-button type="primary">登录</el-button>
+      <el-button type="success">注册</el-button>  
+    </div>
   </div>
 </template>
 
@@ -8,6 +14,8 @@
   #topBar {
     border: 1px solid green;
     min-height: 100px;
+    display: flex;
+    justify-content: space-between;
   }
 </style>
 
