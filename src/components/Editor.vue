9fa9e58f820e4458c42b0043f81e7af6e6358595
liuzhe <zhethink@gmail.com>
2018-09-22 08:19:34 +0800

    先用最笨的方法实现tab切换

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -2,32 +2,32 @@
   <div id="editor">
     <div class="menu">
       <ol>
-        <li class="active">
+        <li v-bind:class="{active: currentTab === 0}" v-on:click="currentTab=0">
           <svg class="icon" aria-hidden="true">
             <use xlink:href="#icon-shenfenzhenghao"></use>
           </svg>
         </li>
-        <li>
+        <li v-bind:class="{active: currentTab === 1}" v-on:click="currentTab=1">
           <svg class="icon" aria-hidden="true">
             <use xlink:href="#icon-workfillmtui"></use>
           </svg>
         </li>
-        <li>
+        <li v-bind:class="{active: currentTab === 2}" v-on:click="currentTab=2">
           <svg class="icon" aria-hidden="true">
             <use xlink:href="#icon-book"></use>
           </svg>
         </li>
-        <li>
+        <li v-bind:class="{active: currentTab === 3}" v-on:click="currentTab=3">
           <svg class="icon" aria-hidden="true">
             <use xlink:href="#icon-code"></use>
           </svg>
         </li>
-        <li>
+        <li v-bind:class="{active: currentTab === 4}" v-on:click="currentTab=4">
           <svg class="icon" aria-hidden="true">
             <use xlink:href="#icon-icon22"></use>
           </svg>
         </li>
-        <li>
+        <li v-bind:class="{active: currentTab === 5}" v-on:click="currentTab=5">
           <svg class="icon" aria-hidden="true">
             <use xlink:href="#icon-phone"></use>
           </svg>
@@ -35,21 +35,37 @@
       </ol>
     </div>
     <div class="editor-content">
-      ners
+      <ol>
+        <li v-bind:class="{active: currentTab === 0}">tab1</li>
+        <li v-bind:class="{active: currentTab === 1}">tab2</li>
+        <li v-bind:class="{active: currentTab === 2}">tab3</li>
+        <li v-bind:class="{active: currentTab === 3}">tab4</li>
+        <li v-bind:class="{active: currentTab === 4}">tab5</li>
+        <li v-bind:class="{active: currentTab === 5}">tab6</li>
+      </ol>
     </div>
   </div>
 </template>
 
+<script>
+export default {
+  data(){
+    return {
+      currentTab: 0
+    }
+  }
+}
+</script>
+
+
 <style lang="scss">
   #editor {
-    border: 1px solid black;
     min-height: 100px;
     display: flex;
     > div {
       border: 1px solid;
     }
     .menu{
-      border-color: red;
       li {
         padding: 16px;
         font-size: 24px;
@@ -67,6 +83,12 @@
     }
     .editor-content{
       flex: 1;
+      li {
+        display: none;
+        &.active{
+          display: block;
+        }
+      }
     }
   }
 </style>
